b81f0ab25e4461a54883a3cba023143b7ad4035c
Nigel <nperis27@gmail.com>
2025-10-20 23:10:15 +0530

    feat: add metadata

--- src/app/(site)/documentation/page.tsx
+++ src/app/(site)/documentation/page.tsx
@@ -3,7 +3,46 @@ import { Documentation } from '@/app/components/Documentation/Documentation';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Featurs | SiEducational',
+  title: 'Vidyanugraha Education Trust',
+  description:
+    'Vidyanugraha Education Trust delivers foundational education with a human touch, offering faculty recruitment and management services.',
+  keywords: [
+    'Vidyanugraha',
+    'Moodbidri',
+    'Mangalore Faculty Team',
+    'NEET',
+    'JEE',
+    'CET',
+    'Coaching',
+    'PUC',
+  ],
+  authors: [
+    { name: 'Vidyanugraha Education Trust', url: 'https://vidyanugraha.com' },
+  ],
+  openGraph: {
+    title: 'Vidyanugraha Education Trust',
+    description:
+      'Vidyanugraha Education Trust delivers foundational education with a human touch, offering faculty recruitment and management services.',
+    url: 'https://vidyanugraha.com',
+    siteName: 'Vidyanugraha Education Trust',
+    images: [
+      {
+        url: 'https://vidyanugraha.com/vidyanugraha-logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Vidyanugraha Education Trust',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Vidyanugraha Education Trust',
+    description:
+      'Vidyanugraha Education Trust delivers foundational education with a human touch, offering faculty recruitment and management services.',
+    images: ['https://vidyanugraha.com/vidyanugraha-logo.png'],
+  },
 };
 
 export default function Page() {
